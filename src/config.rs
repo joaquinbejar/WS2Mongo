2adedebb9aff0af1ad85cd20964e916caf517d4d
Joaquin Bejar <jb@taunais.com>
2024-05-17 06:50:00 +0200

    update documentation

--- src/config.rs
+++ src/config.rs
@@ -25,43 +25,101 @@ use serde_json::json;
 use std::env;
 use thiserror::Error;
 
+/// Represents the configuration options for the application.
 #[derive(Debug, Clone)]
 pub struct Config {
+    /// The WebSocket URL for connecting to the server.
     pub websocket_url: String,
+
+    /// Optional API key for WebSocket authentication.
     pub websocket_api_key: Option<String>,
+
+    /// Optional API secret for WebSocket authentication.
     pub websocket_api_secret: Option<String>,
+
+    /// The MongoDB URI for connecting to the database.
     pub mongodb_uri: String,
+
+    /// The name of the database to use.
     pub database_name: String,
+
+    /// The name of the collection within the database.
     pub collection_name: String,
+
+    /// Optional username for MongoDB authentication.
     pub mongodb_user: Option<String>,
+
+    /// Optional password for MongoDB authentication.
     pub mongodb_password: Option<String>,
+
+    /// Optional authentication source for MongoDB.
     pub mongodb_auth_source: Option<String>,
+
+    /// Optional authentication mechanism for MongoDB.
     pub mongodb_auth_mechanism: Option<String>,
 }
 
+/// An enum representing various errors that can occur during configuration.
 #[derive(Error, Debug)]
 pub enum ConfigError {
+    /// Error indicating that a required environment variable is missing.
     #[error("missing environment variable: {0}")]
     MissingEnvVar(String),
 }
 
 impl Config {
+    /// Creates a new `Config` instance by reading environment variables.
+    ///
+    /// # Errors
+    ///
+    /// Returns a `ConfigError` if a required environment variable is missing.
     pub fn new() -> Result<Self, ConfigError> {
         Ok(Config {
-            websocket_url: env::var("WEBSOCKET_URL").unwrap_or("ws://localhost:5678".to_string()),
+            websocket_url: Self::get_env_var_or_default("WEBSOCKET_URL", "ws://localhost:5678".to_string()),
             websocket_api_key: env::var("WEBSOCKET_API_KEY").ok(),
             websocket_api_secret: env::var("WEBSOCKET_API_SECRET").ok(),
-            mongodb_uri: env::var("MONGODB_URI").unwrap_or("mongodb://localhost:27017".to_string()),
-            database_name: env::var("DATABASE_NAME")
-                .map_err(|_| ConfigError::MissingEnvVar("DATABASE_NAME".to_string()))?,
-            collection_name: env::var("COLLECTION_NAME")
-                .map_err(|_| ConfigError::MissingEnvVar("COLLECTION_NAME".to_string()))?,
+            mongodb_uri: Self::get_env_var_or_default("MONGODB_URI", "mongodb://localhost:27017".to_string()),
+            database_name: Self::get_env_var_or_error("DATABASE_NAME")?,
+            collection_name: Self::get_env_var_or_error("COLLECTION_NAME")?,
             mongodb_user: env::var("MONGODB_USER").ok(),
             mongodb_password: env::var("MONGODB_PASSWORD").ok(),
             mongodb_auth_source: env::var("MONGODB_AUTH_SOURCE").ok(),
             mongodb_auth_mechanism: env::var("MONGODB_AUTH_MECHANISM").ok(),
         })
     }
+
+    /// Gets the value of an environment variable or returns a default value if the variable is not set.
+    ///
+    /// # Arguments
+    ///
+    /// * `var_name` - The name of the environment variable.
+    /// * `default_value` - The default value to use if the environment variable is not set.
+    fn get_env_var_or_default(var_name: &str, default_value: String) -> String {
+        env::var(var_name).unwrap_or(default_value)
+    }
+
+    /// Gets the value of an environment variable or returns an error if the variable is not set.
+    ///
+    /// # Arguments
+    ///
+    /// * `var_name` - The name of the environment variable.
+    ///
+    /// # Errors
+    ///
+    /// Returns a `ConfigError::MissingEnvVar` if the environment variable is not set.
+    fn get_env_var_or_error(var_name: &str) -> Result<String, ConfigError> {
+        env::var(var_name).map_err(|_| ConfigError::MissingEnvVar(var_name.to_string()))
+    }
+
+    /// Serializes the configuration to a JSON string.
+    ///
+    /// # Returns
+    ///
+    /// Returns a JSON string representation of the configuration.
+    ///
+    /// # Errors
+    ///
+    /// Returns a `serde_json::Error` if serialization fails.
     pub fn print_as_json(&self) -> serde_json::Result<String> {
         let json_config = json!({
             "WEBSOCKET_URL": self.websocket_url,
